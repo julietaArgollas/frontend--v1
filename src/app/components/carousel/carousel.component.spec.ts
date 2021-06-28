74e56adbcdf7094008d75d735d1ffc85caf6cc11
julietaArgollas <julieta.c.argollas@gmail.com>
2021-06-28 16:28:57 -0400

    carousel

--- src/app/components/carousel/carousel.component.spec.ts
+++ src/app/components/carousel/carousel.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CarouselComponent } from './carousel.component';
 
@@ -6,12 +6,12 @@ describe('CarouselComponent', () => {
   let component: CarouselComponent;
   let fixture: ComponentFixture<CarouselComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [ CarouselComponent ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CarouselComponent);
--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class CarouselComponent implements OnInit {
+  //images = [1, 2, 3, 4].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  images=[];
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 2000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = true;
+  }
 
   ngOnInit(): void {
+    this.images[1]="../../assets/imagen1.jpg"
+    this.images[2]="../../assets/imagen2.jpg"
+    this.images[3]="../../assets/imagen3.jpg"
   }
 
 }
