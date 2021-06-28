93ba346580febad2eb748b94ce3350162b4b32c2
alvarofloresf <alvaro.fafy98@gmail.com>
2021-06-28 15:34:32 -0400

    carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    CarruselComponent
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
